7f03b226f6eaef925a39c997946bdc6d8b1823b0
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-08-28 17:28:02 -0300

    fix event_date collumn name

--- app/repositories/event_repository.py
+++ app/repositories/event_repository.py
@@ -10,20 +10,23 @@ class EventRepository:
 
     def create(self, event: Event) -> Event:
         insert_event_query = """
-            INSERT INTO events (title, description, date)
-            VALUES (%s, %s, %s) RETURNING id
+            INSERT INTO events (title, description, event_date)
+            VALUES (%s, %s, %s)
+            RETURNING id
         """
-        event.id = self._db_service.execute_query(insert_event_query, (event.title, event.description, event.date))
+        event.id = self._db_service.execute_query(
+            insert_event_query, (event.title, event.description, event.date)
+        )[0]['id']
         return event
 
     def find_all(self) -> list[Event]:
         return self._db_service.fetch_all("""
-            SELECT id, title, description, date FROM events
+            SELECT id, title, description, event_date FROM events
         """)
 
     def find_by_id(self, event_id: int) -> Event | None:
         return self._db_service.fetch_one("""
-            select id, title, description, date from events where id = %s
+            select id, title, description, event_date from events where id = %s
         """, (event_id,))
 
     def delete(self, event_id: int) -> bool:
