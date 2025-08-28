7e9a804b689023277a1beef5000361c90fdb2215
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-08-28 17:27:45 -0300

    create init for Evnt class

--- app/models/event_model.py
+++ app/models/event_model.py
@@ -7,5 +7,10 @@ class Event:
     description: str
     date: datetime
 
+    def __init__(self, title: str, description: str, date: datetime):
+        self.title = title
+        self.description = description
+        self.date = date
+
 
 __all__ = ['Event']
