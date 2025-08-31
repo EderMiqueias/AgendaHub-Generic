ff540f693909af53a78bc05326620bf34c65083e
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2025-08-31 00:31:07 -0300

    add EventPydantic class model

--- app/models/event_model.py
+++ app/models/event_model.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+from typing import Union
+
+from pydantic import BaseModel
 
 
 class Event:
     id: int | None = None
     title: str
     description: str
-    date: datetime
+    event_date: Union[datetime, str]
 
     def __init__(self, title: str, description: str, date: datetime):
         self.title = title
@@ -13,4 +16,8 @@ class Event:
         self.date = date
 
 
-__all__ = ['Event']
+class EventPydantic(BaseModel, Event):
+    pass
+
+
+__all__ = ['Event', 'EventPydantic']
