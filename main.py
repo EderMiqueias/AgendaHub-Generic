d4e70145fc2c52404adc272c3f8074c43e40a80c
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-02-08 17:18:26 -0300

    adjusts main.py file

--- main.py
+++ main.py
@@ -6,18 +6,30 @@ import uvicorn
 
 framework = os.getenv("APP_FRAMEWORK", "pebarest").lower()
 
-if framework == "fastapi":
-    from run_fastapi import app
-elif framework == "flask":
-    from run_flask import asgi_app as app
-elif framework == "pebarest":
-    from run_pebarest import app
-    with make_server('', 8000, app) as httpd:
-        print("Server listening on http://127.0.0.1:8000")
-        httpd.serve_forever()
-else:
-    raise RuntimeError(f"Framework inválido: {framework}")
 
-
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+if __name__ == "__main__":
+    if framework == "fastapi":
+        from run_fastapi import app
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    elif framework == "flask":
+        from run_flask import asgi_app as app
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    elif framework == "pebarest":
+        from run_pebarest import app
+        with make_server('', 8000, app) as httpd:
+            # Generate tests for the application
+            print("Creating tests for application...")
+            app.generate_tests({
+                '/greeting': {
+                    'GET': None,
+                    'POST': {
+                        "title": "School Meeting",
+                        "description": "Meeting to discuss the school schedule",
+                        "event_date": "2023-10-01T14:00:00Z"
+                    }
+                }
+            }, output_file="tests/test_events_api.py")
+            print("Server listening on http://127.0.0.1:8000")
+            httpd.serve_forever()
+    else:
+        raise RuntimeError(f"Framework inválido: {framework}")
