9fac27b03a776c7e873dd48c1849befa5ac35ab6
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-02-09 12:30:46 -0300

    add falcon to main file

--- main.py
+++ main.py
@@ -14,6 +14,14 @@ if __name__ == "__main__":
     elif framework == "flask":
         from run_flask import asgi_app as app
         uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    elif framework == "falcon":
+        from run_falcon import get_app
+
+        app = get_app()
+        print("Falcon Server (WSGI) listening on http://127.0.0.1:8000")
+
+        with make_server('', 8000, app) as httpd:
+            httpd.serve_forever()
     elif framework == "pebarest":
         from run_pebarest import app
         with make_server('', 8000, app) as httpd:
