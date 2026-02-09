d0caaf7dccd02c78e22af22acea881110db3f621
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-02-09 12:17:08 -0300

    add get falcon app controller to __init__

--- app/controllers/event_controllers/__init__.py
+++ app/controllers/event_controllers/__init__.py
@@ -1,3 +1,4 @@
 from .fast_api_controller import get_app as app_fastapi
 from .flask_controller import get_app as app_flask
 from .peba_rest_controller import get_app as app_pebarest
+from .falcon_controller import get_app as app_falcon
